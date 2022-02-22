82941347ce1851a586ee04d15988ebc14b3e2b26
Priyanka Adsul <Priyankaadsul18@gmail.com>
2022-02-22 23:47:46 +0000

    Create Array12_removeDuplicatedFromSortedArray.java

---
+++ src/main/java/Array12_removeDuplicatedFromSortedArray.java
@@ -0,0 +1,22 @@
+//Third most popular question
+//avoid spelling mistakes and using arr instead of nums
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int index =1; //consider first element to be always unique
+        
+        for (int i=0; i< nums.length -1 ; i++){
+            
+            if(nums[i+1]!=nums[i]){ 
+                //as this is a sorted array - increase the index only if there is a mismatch
+                nums[index++]=nums[i+1]; //put the mismatched element at index and increase the index
+            }
+            
+        }
+        
+        return index; 
+        //index will be equal to length of the new unique array - remember it started at 1 - so need to do +1
+        
+    }
+}
