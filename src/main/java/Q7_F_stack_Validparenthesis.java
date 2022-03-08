509329d4856ab3d41df73b6a4834d36f60d4ae8b
Priyanka Adsul <Priyankaadsul18@gmail.com>
2022-03-08 22:10:39 +0000

    Create Q7_F_stack_Validparenthesis.java

---
+++ src/main/java/Q7_F_stack_Validparenthesis.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        //Always even
+        if (s.length()%2 !=0) return false;
+        
+        //define stack
+        Stack<Character> stack = new Stack();
+        
+        //Convert string to char array
+        for (char c:s.toCharArray()){            
+            if (c == '(' || c == '{' || c == '['){
+                stack.push(c); //if opening bracket push it
+            }else if (c == ')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop(); // pop it only if the peek matches the opposite one            
+            }else if (c == '}' && !stack.isEmpty() && stack.peek() == '{'){
+                stack.pop();                
+            }else if (c == ']' && !stack.isEmpty() && stack.peek() == '['){
+                stack.pop();                
+            }else{
+                return false; // important in case of [(}})]
+            }
+        }
+        
+        //At the end the stack should be empty
+        return stack.isEmpty();
+    }
+}
