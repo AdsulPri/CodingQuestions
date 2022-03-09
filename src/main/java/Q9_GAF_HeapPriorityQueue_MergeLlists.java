91344f3af27f8bde87474cd093ef405f0d557ef0
Priyanka Adsul <Priyankaadsul18@gmail.com>
2022-03-09 22:01:12 +0000

    Update Q9_GAF_HeapPriorityQueue_MergeLlists.java

--- src/main/java/Q9_GAF_HeapPriorityQueue_MergeLlists.java
+++ src/main/java/Q9_GAF_HeapPriorityQueue_MergeLlists.java
@@ -5,7 +5,7 @@
 
 WITH HEAP - https://www.youtube.com/watch?v=zLcNwcR6yO4
 
-
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -17,7 +17,8 @@ WITH HEAP - https://www.youtube.com/watch?v=zLcNwcR6yO4
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        
+  
+        /* Not optimistic
         //define min heap
         PriorityQueue<Integer> minHeap= new PriorityQueue<>();
         
@@ -40,6 +41,50 @@ class Solution {
         }
         
         return dummy.next;
+        
+        */
+        
+        //optimised with divide and conquer
+        if(lists == null || lists.length == 0 ) return null;
+        
+        //recursive function for merging lists on the basis of mid value - divide and conquer
+        return mergeMyLists(lists, 0, lists.length - 1);
+        
+        
+    }
+    
+    
+    private ListNode mergeMyLists (ListNode[] lists, int start, int end){
+        
+        if (start == end) return lists[start];
+        
+        int mid = start + (end - start)/2; //start+end/2
+        ListNode left = mergeMyLists (lists, start, mid); //till mid
+        ListNode right = mergeMyLists(lists, mid+1, end); //after mid
+        return merge(right, left);      //merge to get results 
+    }
+    
+    private ListNode merge(ListNode l, ListNode r){
+        
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        while(l!=null || r!=null){//dont forget while condition
+                if(l==null){
+                    current.next = r;
+                    r=r.next;
+                }else if (r==null){
+                    current.next = l;
+                    l=l.next;
+                } else if(l.val < r.val ){
+                    current.next = l;
+                    l=l.next;
+                }else{
+                    current.next = r;
+                    r=r.next;
+                }
+                current = current.next; //important
+        }
+        return result.next;      
         
     }
 }
