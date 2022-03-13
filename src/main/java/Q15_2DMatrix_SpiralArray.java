cf59ea655ce024e3e5ace246301992ce9032eb23
Priyanka Adsul <Priyankaadsul18@gmail.com>
2022-03-13 02:03:54 +0000

    Create Q15_2DMatrix_SpiralArray.java

---
+++ src/main/java/Q15_2DMatrix_SpiralArray.java
@@ -0,0 +1,58 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+       List<Integer> res = new ArrayList<>();
+        
+        if(matrix == null || matrix.length == 0)
+            return res;
+            
+        //matrix [ROW][COL]
+        int rStart = 0;
+        int rEnd = matrix.length - 1;
+        int cStart = 0;
+        int cEnd = matrix[0].length - 1;
+        
+        
+        while(cStart <= cEnd && rStart <= rEnd ){
+            
+            // forward row RIGHT
+            
+            for (int i = cStart; i<= cEnd ; i++){
+                res.add(matrix[rStart][i]);
+            }
+            
+            rStart++;
+            
+            // downward column DOWN
+            
+            for (int i = rStart; i<= rEnd ; i++){
+                res.add(matrix[i][cEnd]);
+            }
+            
+            cEnd--;
+            
+            // backward row LEFT
+             
+            if( rStart <= rEnd){ //Important condition
+                for (int i = cEnd; i>= cStart ; i--){
+                res.add(matrix[rEnd][i]);
+            }
+            }
+            
+            rEnd-- ;
+            
+            
+            // upward column UP
+           if(cStart <=cEnd){ //Important condition
+                for (int i = rEnd; i>= rStart ; i--){
+                res.add(matrix[i][cStart]);
+             }
+            }
+            
+            cStart++;  
+        }
+        return res;
+        
+        
+    }
+}
