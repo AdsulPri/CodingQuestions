c97c8e379f88bbf4f9367b752e2d479cdb77214c
Priyanka Adsul <Priyankaadsul18@gmail.com>
2022-03-13 00:51:01 +0000

    Update Q12_2DArray_matrix_rotateimage.java

--- src/main/java/Q12_2DArray_matrix_rotateimage.java
+++ src/main/java/Q12_2DArray_matrix_rotateimage.java
@@ -6,9 +6,9 @@ class Solution {
         
         int N = matrix.length;
         
-        //transpose
+        //transpose - diagonal remains same
         for (int i =0 ; i < N; i++){
-            for (int j = i ; j< N ; j++){
+            for (int j = i ; j< N ; j++){ //j starts with i
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -16,11 +16,11 @@ class Solution {
         }
         
         
-        //flip
+        //flip - first column with last column 
         for (int i =0 ; i < N; i++){
-            for (int j = 0 ; j< N/2 ; j++){
+            for (int j = 0 ; j< N/2 ; j++){ //goes till only N/2
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][N-1-j];
+                matrix[i][j] = matrix[i][N-1-j]; //from last element N-1-j
                 matrix[i][N-1-j] = temp;                
             }
         }
