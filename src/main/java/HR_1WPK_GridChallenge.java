45aaeccb01f5f919a430324e5c63cd0ee7e201e6
Priyanka Adsul <Priyankaadsul18@gmail.com>
2022-03-29 00:05:03 +0100

    Create HR_1WPK_GridChallenge.java

---
+++ src/main/java/HR_1WPK_GridChallenge.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gridChallenge' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING_ARRAY grid as parameter.
+     */
+
+    public static String gridChallenge(List<String> grid) {
+    // Write your code here
+    ArrayList<String> al = new ArrayList<>();
+    
+    for(String s:grid){
+        al.add(sortIt(s));
+    }
+    
+    for (int i=1; i < al.size(); i++){
+        for (int j=0; j< al.get(0).length(); j++){
+            if(al.get(i-1).charAt(j)>al.get(i).charAt(j)){
+                return "NO";
+            }
+        }
+    }
+    return "YES";
+    }
+    
+    public static String sortIt(String s){
+        char[] temp = s.toCharArray();
+        
+        Arrays.sort(temp);
+        
+        return new String(temp); //Important to prepare new string
+    }
+
+}
+//https://www.hackerrank.com/challenges/one-week-preparation-kit-grid-challenge/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-four
+//https://www.youtube.com/watch?v=riUJucy5FZU
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<String> grid = IntStream.range(0, n).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String result = Result.gridChallenge(grid);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
